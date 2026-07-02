8696d6599d29046d0f78ea542fa847e183cc7b81
Camilo Andres <camilo@MacBook-Air-de-Camilo.local>
2026-07-02 16:00:06 -0500

    fiz provider

--- terraform/environments/devel/main.tf
+++ terraform/environments/devel/main.tf
@@ -9,6 +9,12 @@ terraform {
   required_version = ">= 1.2"
 }
 
+provider "aws" {
+  region     = "us-east_1"
+  access_key = ${ secrets.AWS_ACCESS_KEY }
+  secret_key = ${ secrets.AWS_SECRET_ACCESS_KEY }
+}
+
 module "deploy_infra" {
   source      = "../../modules/app/"
 }
